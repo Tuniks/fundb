506934f33199365e1fcb093e604b387224877e14
joaohenrique <joaohenriquef@poli.ufrj.br>
2017-12-04 21:53:21 -0200

    Populate data.js with places

--- data.js
+++ data.js
@@ -31,6 +31,38 @@ export default data = {
         },
     ],
 
+    places:[
+        {key:0, 
+            name:'Nárnia', 
+            rating:[5,3], 
+            type:[false,true,false,true,true], 
+            img:require('./img/burgues-safado.jpg'),
+            reviews:[
+                {key:0, namerev:'johnny', review:'sem gosto e caro naum gosto so aceita credito.'},
+                {key:1, namerev:'busta', review:'tem q ser saudavel filho'}]
+        },
+        {key:1, 
+            name:'Terraço CT', 
+            rating:[3,3], 
+            type:[true,false,false,true,true], img:require('./img/yakuza.jpg'),
+            reviews:[
+                {key:0, namerev:'formiga', review:'adoro gente sério mto bom'},
+                {key:1, namerev:'kojima', review: '私はそれがとても好き'}]
+        },
+        {key:2, 
+            name:'Prefeitura',
+            rating:[1,5], 
+            type:[true,true,true,true,true], 
+            img:require('./img/kanye.jpg'),
+            reviews:[
+                {key:0, namerev: 'Antonio', review: 'Cara gnt esse cara é top demais namoral como assim. Eu sei q to chovendo no molhado, mas ele merece.'},
+                {key:1, namerev: 'Resto do Mundo', review: 'true dat review de cima.'},
+                {key:2, namerev: 'Resto da Galaxia', review: 'põe true nisso.'},
+                {key:3, namerev: 'Anthony Fandango', review: 'acho overrated, 6/10'},
+            ]
+        },
+    ],
+
     teachers:[
         {key:0, 
             name:'Cookie', 
